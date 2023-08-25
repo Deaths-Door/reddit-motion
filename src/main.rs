47caac552ab1212adef78563882e5a116158985a
Deaths-Door <aaravaditya51@gmail.com>
2023-08-25 11:29:32 +0200

    Initial Message

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![doc = include_str!("../README.md")]
+
+mod setup;
+
+fn main(){
+    setup::print_initial_messages()
+
+    let setup = 
+}
---
+++ src/setup.rs
@@ -0,0 +1,17 @@
+
+pub(in crate) fn print_initial_messages() {
+    const REDDIT_MOTION : &str = r#"
+    ____           __    ___ __     __  ___      __  _           
+   / __ \___  ____/ /___/ (_) /_   /  |/  /___  / /_(_)___  ____ 
+  / /_/ / _ \/ __  / __  / / __/  / /|_/ / __ \/ __/ / __ \/ __ \
+ / _, _/  __/ /_/ / /_/ / / /_   / /  / / /_/ / /_/ / /_/ / / / /
+/_/ |_|\___/\__,_/\__,_/_/\__/  /_/  /_/\____/\__/_/\____/_/ /_/ 
+          
+Thanks for using this tool! 
+Feel free to contribute to this project on GitHub! 
+If you have any questions, feel free to join my Discord server or submit a GitHub issue. 
+You can find solutions to many common problems in the documentation: https://docs.rs/reddit-motion
+
+"# ;
+    println!("{REDDIT_MOTION}");
+}
