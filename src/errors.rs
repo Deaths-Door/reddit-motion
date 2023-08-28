aec3a219030ddc5df237a5b60e81de0e43f6a01e
Deaths-Door <aaravaditya51@gmail.com>
2023-08-28 18:34:59 +0200

    Downlods yt vids i thinkg

--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,16 @@
 use thiserror::Error;
 
 #[derive(Error,Debug)]
-pub(in crate) enum AssetsError {
+pub enum AssetsError {
     #[error("Failed to create asset directory : {}",.0)]
     CreatingDirectory(#[from] std::io::Error),
 
     #[error("Failed to find video stream , maybe check for internet connection")]
     VideoStreamUnavailable,
 
-
     #[error("Failed to download video due to , {}",.0)]
-    DownloadError(#[from] rustube::Error)
+    DownloadError(#[from] rustube::Error),
+
+    #[error("Failed to join threads downloading the assets")]
+    ErrorJoiningThreads
 }
